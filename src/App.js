fa72d275c376afdeb4ad782fc8584e090927905e
unknown <liuchenglu535.gmail.com>
2020-03-26 01:04:41 +0800

    学习redux中

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react'
-import style from './App.less'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom"
+// import React from 'react'
+// import style from './App.less'
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+//     Link
+// } from "react-router-dom"
 
-import User from './pages/User'
-import Article from './pages/Article'
-import BasicLayout from "@/layout/BasicLayout/BasicLayout"
-function App() {
-    return (
-        <Router>
-            <Route path="/">
-                <BasicLayout></BasicLayout>
-            </Route>
-        </Router>
-    )
-}
+// import User from './pages/User'
+// import Article from './pages/Article'
+// import BasicLayout from "@/layout/BasicLayout/BasicLayout"
+// function App() {
+//     return (
+//         <Router>
+//             <Route path="/">
+//                 <BasicLayout></BasicLayout>
+//             </Route>
+//         </Router>
+//     )
+// }
+
+// export default App
+
+import React from 'react'
+import Footer from '@/components/Footer'
+import AddTodo from '@/redux/containers/AddTodo'
+import VisibleTodoList from '@/redux/containers/VisibleTodoList'
+
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '@/App'
-import './coverage.less'
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from '@/App'
+// import './coverage.less'
 
-/*
-    入口文件
-*/
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
+// /*
+//     入口文件
+// */
+// ReactDOM.render(
+//     <App/>,
+//     document.getElementById('root')
+// )
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from '@/redux/reducers'
+import App from '@/App'
+
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 )
